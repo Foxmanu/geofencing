fee8f42547102f90c7d3154b7cc529473c41394c
Foxmanu <sjm7008@gmail.com>
2026-04-13 18:23:31 +0530

    asss212

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  server: {
-    host: '0.0.0.0',   // accessible from other PCs on the same network
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    }
-  }
 })
